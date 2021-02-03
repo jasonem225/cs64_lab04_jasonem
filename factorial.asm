7aa0106d149a548fa50a22c1c8077edd71515129
Jason Em <jasonem@cs.ucsb.edu>
2021-02-02 21:48:29 -0800

    version 2

--- factorial.asm
+++ factorial.asm
@@ -13,13 +13,60 @@
 #
 .data
 
-	# TODO: Write your initializations here
+input: .asciiz "Enter a number:\n"
+output1: .asciiz "Factorial of "
+output2: .asciiz " is:\n"
+nl: .asciiz "\n"
 
 #Text Area (i.e. instructions)
 .text
 main:
 
-	# TODO: Write your code here
+    li $v0, 4
+    la $a0, input
+    syscall
+
+    li $v0, 5
+    syscall
+    move $t0, $v0
+
+    li $t1, 2
+    li $t2, 1
+    li $t4, 1
+
+LOOP:
+
+   
+    sltu $t3, $t4, $t0
+    beq $t3, $zero, PRINT
+    multu $t2, $t1
+    mflo $t2
+    addiu $t1, $t1, 1
+    addiu $t4, $t4, 1
+    j LOOP
+
+PRINT:
+
+    li $v0, 4
+    la $a0, output1
+    syscall
+
+    li $v0, 1
+    move $a0, $t0
+    syscall
+
+    li $v0, 4
+    la $a0, output2
+    syscall
+
+    li $v0, 1
+    move $a0, $t2
+    syscall
+
+    li $v0, 4
+    la $a0, nl
+    syscall
 
 exit:
-	# TODO: Write code to properly exit a SPIM simulation
+    li $v0, 10
+    syscall
